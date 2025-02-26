0d3c981ed9e0587218c04925fa1c222078771399
morchid65 <foitihj@gmail.com>
2025-02-26 21:39:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Code César</title>
-<style>
-    @import url('code_cesar.css');
-    @import url("https://cdn.jsdelivr.net/npm/intro.js@6.0.0/introjs.css");
-    #formulaire {
-        width: 400px;
+console.log("Hello")
+// Déclaration des variables et des fonctions
+let phrase=document.querySelector("#phrase")
+let cle=document.querySelector("#cle")
+let btnCoder=document.querySelector("#btnCoder")
+let txtCode=document.querySelector("#txtCode")
+// Pour mémoire char.charCodeAt(0) et String.fromCharCode(ASCIICode)
+function Coder() {
+    let phraseACoder = phrase.value.toUpperCase() // sur un objet, on peut afficher ses propriétés
+let phraseCodee="" // déclaration d'une variable vide pour créer le code César
+let c
+for (let index = 0; index < phraseACoder.length; index++) {
+    c = phraseACoder[index].charCodeAt(0)
+    if (c != 32) {
+        c = c+cle.value*1
+        phraseCodee = phraseCodee + String.fromCharCode((c-65)%26+65)
+        } else {
+        phraseCodee = phraseCodee + String.fromCharCode(c)
+        }
+    } // cle.value est un texte à convertir
+phraseCodee = phraseCodee + String.fromCharCode(c)
+console.log(c, String.fromCharCode(c))
+}
+txtCode.value = phraseCodee
+// Création des événements
+btnCoder.addEventListener("click", Coder)
+txtCode.value = phraseCodee
+phrase.value = phraseACoder
+function Aider() {
+    introJs().start()
     }
-    label {
-        display: inline-block;
-        min-width: 100px;
-        background-color: rgb(49, 19, 97);
-        margin: 5px 0px 0px 0px;
-        padding: 3px;
-        border-radius: 20px 0px 0px 20px;
+btnAider.addEventListener("click", Aider)
+if (Verifier(phraseACoder)==true || cle.value*1 < 0) {    let c=0
+    for (let index=0; index < phrase.length; index++) {
+    c = phrase.charCodeAt(index)
+    if ((c == 32) || (c > 64 && c < 91) || (c > 47 && c < 58)) {
+    // c'est bon on continue
+    } else {
+    return false
     }
-    input {
-        border-radius: 0px 20px 20px 0px;
-        margin: 5px 0px 0px 0px;
-        padding: 3px;
     }
-    #btnCoder {
-        width: 100%;
+    return true
     }
-</style>
-</head
